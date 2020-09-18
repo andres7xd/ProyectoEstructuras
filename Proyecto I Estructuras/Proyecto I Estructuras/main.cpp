3027bb591aa5b659a7224b344f4ea1c3045304a7
Avdreiller <64115743+Avdreiller@users.noreply.github.com>
2020-09-18 00:38:30 -0600

    Colores

--- Proyecto I Estructuras/Proyecto I Estructuras/main.cpp
+++ Proyecto I Estructuras/Proyecto I Estructuras/main.cpp
@@ -1,40 +1,82 @@
 
 #include "Pila.h"
 using namespace std;
-
-MenuInicio* m = new MenuInicio();
-void guardarBola(Pila*& p, sf::Sprite sprite, sf::CircleShape shape, float radious, float imageWidth, float imageHeight2, Tubo* aux ) {
-    if (p->top() != NULL)
-    {
-        shape.setPosition(sprite.getPosition().x + imageWidth - radious, (p->top()->getShape()).getPosition().y - 57);
-        p->push(shape, aux->getColor());
-    }
-    else {
-        shape.setPosition(sprite.getPosition().x + imageWidth - radious, sprite.getPosition().y + imageHeight2 - radious - 32);
-        p->push(shape, aux->getColor());
-    }
-    p->setCantBol(p->getCantBol() + 1);
-}
 Pila* pila = new Pila();
 Pila* pila2 = new Pila();
 Pila* pila3 = new Pila();
-void devolverBola(int tubo, sf::Sprite sprite2, sf::Sprite sprite, sf::Sprite sprite3, sf::CircleShape shape, float radious, float imageWidth, float imageHeight2, Tubo* aux) {
+float imageWidth = 0;
+float radious = 0;
+float imageHeight = 0;
+float imageHeight2 = 0;
+
+MenuInicio* m = new MenuInicio();
+
+
+void guardarBola(Pila*& p, sf::Sprite sprite, sf::CircleShape shape, string color ) {
+    if (p->top() != NULL)
+    {
+        shape.setPosition(sprite.getPosition().x + imageWidth - radious, (p->top()->getShape()).getPosition().y - 57);
+        p->push(shape, color);
+    }
+    else {
+        shape.setPosition(sprite.getPosition().x + imageWidth - radious, sprite.getPosition().y + imageHeight2 - radious - 32);
+        p->push(shape, color);
+    }
+    p->setCantBol(p->getCantBol() + 1);
+}
+
+void devolverBola(int tubo, sf::Sprite sprite2, sf::Sprite sprite, sf::Sprite sprite3, sf::CircleShape shape, Tubo* aux) {
     if (tubo == 1)
     {
-        guardarBola(pila, sprite2, shape, radious, imageWidth, imageHeight2, aux);
+        guardarBola(pila, sprite2, shape, aux->getColor());
     }
     else {
         if (tubo == 2)
         {
-            guardarBola(pila2, sprite, shape, radious, imageWidth, imageHeight2, aux);
+            guardarBola(pila2, sprite, shape, aux->getColor());
         }
         else {
             if (tubo == 3)
             {
-                guardarBola(pila3, sprite3, shape, radious, imageWidth, imageHeight2, aux);
+                guardarBola(pila3, sprite3, shape, aux->getColor());
             }
         }
     }
+}
+void llenarTubo(Pila*& pila, sf::Sprite sprite, int num)
+{
+    sf::CircleShape shap1(radious);
+    switch (num)
+    {
+    case 1:
+        shap1.setFillColor(sf::Color::Blue);
+        guardarBola(pila, sprite, shap1, "Blue");
+        break;
+    case 2: 
+        shap1.setFillColor(sf::Color::White);
+        guardarBola(pila, sprite, shap1, "White");
+        break;
+    case 3: 
+        shap1.setFillColor(sf::Color::Green);
+        guardarBola(pila, sprite, shap1, "Green");
+        break;
+    case 4:
+        shap1.setFillColor(sf::Color::Red);
+        guardarBola(pila, sprite, shap1, "Red");
+        break;
+    case 5:
+        shap1.setFillColor(sf::Color::Magenta);
+        guardarBola(pila, sprite, shap1, "Magenta");
+        break;
+    case 6:
+        shap1.setFillColor(sf::Color::Yellow);
+        guardarBola(pila, sprite, shap1, "Yellow");
+        break;
+    default: break;
+    }
+    
+    
+ 
 }
 int main()
 {
@@ -51,23 +93,43 @@ int main()
     image.loadFromFile("resourse\\Tubo4.png");
     sf::Sprite sprite(image);
     sprite.setScale(0.5f,0.5f);
-    float imageWidth = (sprite.getTexture()->getSize().x / 2) * 0.5f;
+
+    sf::Texture image2;
+    image2.loadFromFile("resourse\\TuboTope.png");
+    sf::Sprite spriteTope(image2);
+    spriteTope.setScale(0.5f, 0.5f);
+
+    
+
+
+    imageWidth = (sprite.getTexture()->getSize().x / 2) * 0.5f;
     float imageWidth2 = (sprite.getTexture()->getSize().x) * 0.5f;
-    float imageHeight = (sprite.getTexture()->getSize().y / 2) * 0.5f;
-    float imageHeight2 = (sprite.getTexture()->getSize().y) * 0.5f;
+    imageHeight = (sprite.getTexture()->getSize().y / 2) * 0.5f;
+    imageHeight2 = (sprite.getTexture()->getSize().y) * 0.5f;
     float windowWidth = window.getSize().x/2;
     float windowHeight = window.getSize().y/2;
+
+
     sprite.setPosition(windowWidth - imageWidth, windowHeight - imageHeight);
+    spriteTope.setPosition(windowWidth - imageWidth, windowHeight - imageHeight);
 
     sf::Sprite sprite2(image);
     sprite2.setScale(0.5f, 0.5f);
+
+    sf::Sprite spriteTope2(image2);
+    spriteTope2.setScale(0.5f, 0.5f);
     
     sprite2.setPosition(windowWidth - imageWidth-150, windowHeight - imageHeight);
+    spriteTope2.setPosition(windowWidth - imageWidth - 150, windowHeight - imageHeight);
 
     sf::Sprite sprite3(image);
     sprite3.setScale(0.5f, 0.5f);
+
+    sf::Sprite spriteTope3(image2);
+    spriteTope3.setScale(0.5f, 0.5f);
     
     sprite3.setPosition(windowWidth - imageWidth+150, windowHeight - imageHeight);
+    spriteTope3.setPosition(windowWidth - imageWidth + 150, windowHeight - imageHeight);
 
     sf::CircleShape shape2(imageWidth-7);
     shape2.setFillColor(sf::Color::Green);
@@ -84,7 +146,7 @@ int main()
     float mouseX = 0;
     float mouseY = 0;
 
-    float radious = imageWidth - 7;
+    radious = imageWidth - 7;
     //sf::RectangleShape rect;
     
 
@@ -104,45 +166,99 @@ int main()
         window.draw(sprite3);
         window.display();
     }*/
-    for (int i = 0; i < 4; i++)
+    srand(time(0));
+    int num = 0;
+    int nivel = 1;
+    int bol[6];
+    int color[6];
+    for (int i = 0; i < 6; i++)
     {
-        
-
-        if (pila->top()!=NULL)
-        {
-            sf::CircleShape shap1(radious);
-            shap1.setFillColor(sf::Color::Green);
-            shap1.setPosition(sprite2.getPosition().x + imageWidth - radious, (pila->top()->getShape()).getPosition().y - 57);
-            pila->push(shap1, "Green");
-        }
-        else {
-            sf::CircleShape shap1(radious);
-            shap1.setFillColor(sf::Color::Red);
-            shap1.setPosition(sprite2.getPosition().x + imageWidth - radious, sprite2.getPosition().y + imageHeight2 - radious - 32);
-            pila->push(shap1, "Red");
-        }
-        pila->setCantBol(pila->getCantBol() + 1);
+        bol[i] = 0;
+        color[i] = 0;
     }
-
-
-    for (int i = 0; i < 4; i++)
+    int con = 0;
+    bool var = true;
+    bool var2 = false;
+    for (int i = 0; i < nivel+1; i++)
     {
-
-
-        if (pila2->top() != NULL)
+        num = 1 + rand() % 6;
+        var = true;
+        while (var != false)
         {
-            sf::CircleShape shap1(radious);
-            shap1.setFillColor(sf::Color::Red);
-            shap1.setPosition(sprite.getPosition().x + imageWidth - radious, (pila2->top()->getShape()).getPosition().y - 57);
-            pila2->push(shap1, "Red");
+            for (int j = 0; j < 6; j++)
+            {
+                if (color[j] == num) {
+                    var2 = true;
+                }
+            }
+            if (var2==true) {
+                num = 1 + rand() % 6;
+                var2 = false;
+            }
+            else {
+                var = false;
+            }
+        }
+        color[i] = num;
+        //cout<< num << endl;
+    }
+    
+    cout<< color[0];
+    cout<< color[1] << endl;
+    var = true;
+    for (int i = 0; i < 4; i++)
+    {   
+        num = 1 + rand() % 6;
+        var = true;
+        while (var!=false)
+        {
+            if (num!=color[0] && num != color[1]) {
+                num = 1 + rand() % 6;
+            }
+            else {
+                var = false;
+            }
+        }
+        if (num == color[0]) {
+            if (bol[0]<4) {
+                bol[0] = bol[0] + 1;
+            }
         }
         else {
-            sf::CircleShape shap1(radious);
-            shap1.setFillColor(sf::Color::Green);
-            shap1.setPosition(sprite.getPosition().x + imageWidth - radious, sprite.getPosition().y + imageHeight2 - radious - 32);
-            pila2->push(shap1, "Green");
+            if (num == color[1]) {
+                if (bol[1] < 4) {
+                    bol[1] = bol[1] + 1;
+                }
+            }
         }
-        pila2->setCantBol(pila2->getCantBol() + 1);
+        llenarTubo(pila, sprite2, num);
+        cout << num ;
+        num = 1 + rand() % 6;
+        var = true;
+        while (var != false)
+        {
+            if (num != color[0] && num != color[1]) {
+                num = 1 + rand() % 6;
+            }
+            else {
+                var = false;
+            }
+        }
+        if (num == color[0]) {
+            if (bol[0] < 4) {
+                bol[0] = bol[0] + 1;
+            }
+        }
+        else {
+            if (num == color[1]) {
+                if (bol[1] < 4) {
+                    bol[1] = bol[1] + 1;
+                }
+            }
+        }
+        llenarTubo(pila2, sprite, num);
+        cout << num << endl;
+        
     }
 
 
@@ -211,16 +327,16 @@ int main()
                         if (pila->top() != NULL)
                         {
                             if (pila->getCantBol() < 4 && aux->getColor() == pila->top()->getColor()) {
-                                guardarBola(pila, sprite2, shape2, radious, imageWidth, imageHeight2, aux);
+                                guardarBola(pila, sprite2, shape2, aux->getColor());
                             }
                             else
                             {
-                                devolverBola(tubo, sprite2, sprite, sprite3, shape2, radious, imageWidth, imageHeight2, aux);
+                                devolverBola(tubo, sprite2, sprite, sprite3, shape2, aux);
                             }
                         }
                         else
                         {
-                            guardarBola(pila, sprite2, shape2, radious, imageWidth, imageHeight2, aux);
+                            guardarBola(pila, sprite2, shape2, aux->getColor());
                         }
                         
                         tubo = 0;
@@ -232,16 +348,16 @@ int main()
                             {
                                 if (pila2->getCantBol() < 4 && aux->getColor() == pila2->top()->getColor())
                                 {
-                                    guardarBola(pila2, sprite, shape2, radious, imageWidth, imageHeight2, aux);
+                                    guardarBola(pila2, sprite, shape2, aux->getColor());
                                 }
                                 else
                                 {
-                                    devolverBola(tubo, sprite2, sprite, sprite3, shape2, radious, imageWidth, imageHeight2, aux);
+                                    devolverBola(tubo, sprite2, sprite, sprite3, shape2, aux);
                                 }
                             }
                             else
                             {
-                                guardarBola(pila2, sprite, shape2, radious, imageWidth, imageHeight2, aux);
+                                guardarBola(pila2, sprite, shape2, aux->getColor());
                             }
                             tubo = 0;
                         }
@@ -252,23 +368,23 @@ int main()
                                 {
                                     if (pila3->getCantBol() < 4 && aux->getColor() == pila3->top()->getColor())
                                     {
-                                        guardarBola(pila3, sprite3, shape2, radious, imageWidth, imageHeight2, aux);
+                                        guardarBola(pila3, sprite3, shape2, aux->getColor());
                                     }
                                     else
                                     {
-                                        devolverBola(tubo, sprite2, sprite, sprite3, shape2, radious, imageWidth, imageHeight2, aux);
+                                        devolverBola(tubo, sprite2, sprite, sprite3, shape2, aux);
                                     }
                                 }
                                 else
                                 {
-                                    guardarBola(pila3, sprite3, shape2, radious, imageWidth, imageHeight2, aux);
+                                    guardarBola(pila3, sprite3, shape2, aux->getColor());
                                 }
                                 tubo = 0;
                             }
                             else {
                                 if (aux != NULL) {
                                     if (tubo != 0) {
-                                        devolverBola(tubo, sprite2, sprite, sprite3, shape2, radious, imageWidth, imageHeight2, aux);
+                                        devolverBola(tubo, sprite2, sprite, sprite3, shape2, aux);
                                         tubo = 0;
                                     }
                                 }
@@ -363,24 +479,18 @@ int main()
         window.draw(shap2);
         window.draw(shap3);
         window.draw(shap4);*/
-        if (mouseClickedReleased == true)
-        {
-            window.draw(shape2);
-        }
-
-        
-        window.draw(sprite);
-        window.draw(sprite2);
-        window.draw(sprite3);
+        window.draw(spriteTope);
+        window.draw(spriteTope2);
+        window.draw(spriteTope3);
 
         Tubo* t = pila->top();
-        while (t!=NULL)
+        while (t != NULL)
         {
             sf::CircleShape shap(0);
             //cout << " ciclo t" << endl;
             shap = t->getShape();
             window.draw(shap);
-            t=t->getAnt();
+            t = t->getAnt();
         }
 
         Tubo* t2 = pila2->top();
@@ -402,6 +512,18 @@ int main()
             window.draw(shap);
             t3 = t3->getAnt();
         }
+
+
+        if (mouseClickedReleased == true)
+        {
+            window.draw(shape2);
+        }
+
+        
+        window.draw(sprite);
+        window.draw(sprite2);
+        window.draw(sprite3);
+
         //cout << " ciclo a" << endl;
         window.display();
     }
